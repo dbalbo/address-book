aa3e7910505dd7064deebec8e79db84eda511e8e
Debbie Balbo & Desire Perrault-Reid <student@epicodus.com>
2015-04-30 15:39:26 -0700

    first spec pass

--- lib/name.rb
+++ lib/name.rb
@@ -1,8 +1,15 @@
 class Name
-    attr_reader(:first, :last)
+  attr_reader(:first, :last)
 
-    define_method(:initialize) do |attributes|
-        @first = attributes.fetch(:first)
-        # @last = attributes.fetch(:last)
-    end
-end
+  define_method(:initialize) do |first, last|
+    @first = first
+    @last = last
+    # @name = first.concat( last)
+   end
+
+   # define_method(:first) do |attributes|
+   #     @first = attributes.fetch(:first)
+   #     @description = attributes.fetch(:description)
+   #
+
+ end
--- spec/name_spec.rb
+++ spec/name_spec.rb
@@ -4,10 +4,36 @@ require('name')
 
 describe('Name') do
 
+  # describe('#first') do
+  #   it('returns the contacts first name') do
+  #      test_name = Name.new({:first => "first_name", :description =>"Debbie"})
+  #     expect(test_name.first()).to(eq('Debbie'))
+  #   end
+  # end
+
   describe('#first') do
     it('returns the contacts first name') do
-       test_name = Name.new({:first => "Debbie"})
+       test_name = Name.new("Debbie", "Balbo")
       expect(test_name.first()).to(eq('Debbie'))
     end
   end
- 
+
+
+  # describe('#last') do
+  #   it('returns the contacts last name') do
+  #     test_name = Name.new("Debbie Balbo")
+  #     expect(test_name.last()).to(eq('Balbo'))
+  #    end
+  #  end
+
+
+
+  # describe('#last') do
+  #   it('returns the contacts last name') do
+  #     test_name = Name.new({:last => 'Balbo'})
+  #     expect(test_name.last()).to(eq('Balbo'))
+  #    end
+  #  end
+
+
+ end
