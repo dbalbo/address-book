2dea3d135208a935ed7fa8ec0a6cc5bc4185a59c
Debbie Balbo & Desire Perrault-Reid <student@epicodus.com>
2015-04-30 17:08:29 -0700

    YAY

---
+++ lib/address.rb
@@ -0,0 +1,10 @@
+class Address
+  attr_reader(:street, :city, :state, :zip)
+
+  define_method(:initialize) do |attributes|
+    @street = attributes.fetch(:street)
+    @city = attributes.fetch(:city)
+    @state = attributes.fetch(:state)
+    @zip = attributes.fetch(:zip)
+   end
+end
---
+++ spec/address_spec.rb
@@ -0,0 +1,15 @@
+require('rspec')
+require('name')
+require('phone')
+require('address')
+
+describe('Address') do
+
+  describe('#street') do
+    it('returns the contacts address') do
+      test_address = Address.new({:street => "1201 Bubble Street", :city => "Portland", :state => "OR", :zip => 95304})
+      expect(test_address.street()).to(eq('1201 Bubble Street'))
+    end
+  end
+
+end
